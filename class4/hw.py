81e8edaff5d4a1fa56fc48912ea11ecc2dce7e84
marcus11141 <marcus8831114@gmail.com>
2024-10-11 19:06:22 +0800

    class4

--- class4/hw.py
+++ class4/hw.py
@@ -13,6 +13,10 @@
 
 """
 
-f = int(input("請輸入華氏溫度:"))
-C = (f - 32) * 5 / 9
-print(f"華氏溫度{f}F等於攝氏溫度{C}C")
+try:
+    f = float(input("請輸入華氏溫度:"))
+except:
+    print("輸入錯誤!")
+else:
+    C = (f - 32) * 5 / 9
+    print(f"華氏溫度{f}F等於攝氏溫度{C}C")
