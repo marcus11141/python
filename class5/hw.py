4049fc322b3c6ce62b12e050ffc49cce630ea9c3
marcus11141 <marcus8831114@gmail.com>
2024-10-18 19:20:55 +0800

    class5

--- class5/hw.py
+++ class5/hw.py
@@ -5,3 +5,14 @@
 """
 
 import turtle as t
+
+t.penup()  # 提筆, 這樣就不會畫線但是可以移動
+
+for i in range(12):
+    t.forward(100)  # 向前移動100個單位
+    t.stamp()  # 蓋一個印章
+    t.backward(100)  # 向後移動100個單位
+    t.right(30)  # 向右移動30度
+
+
+t.done()  # 讓視窗不會關閉
