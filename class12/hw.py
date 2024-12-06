393944898c09609f8394166c879cd77c8bdf425e
marcus11141 <marcus8831114@gmail.com>
2024-12-06 19:46:07 +0800

    class12

--- class12/hw.py
+++ class12/hw.py
@@ -24,33 +24,35 @@ example:
     ```
 """
 
-order = ["1. 新增餐點", "2. 刪除餐點", "3. 顯示餐點"]
-menu = []
+order = []
+
+
 while True:
-    print(order)
-    try:
-        num = int(input("請輸入選擇: "))
-    except:
-        print("請輸入數字: ")
-        continue
+    print(f"目前order={order}")
+    print("1. 新增餐點")
+    print("2. 刪除餐點")
+    print("3. 提交菜單")
+
+    choice = input("請選擇功能(1-3): ")
+    if choice == "1":
+        item = input("請輸入餐點名稱: ")
+        order.append(item)
+    elif choice == "2":
+        item = input("請輸入餐點名稱: ")
+        if item in order:
+            for x in range(order.count(item)):
+                order.remove(item)
+    elif choice == "3":
+        items = (
+            []
+        )  # order=["漢堡", "薯條", "可樂", "可樂", "可樂"] -> items=["漢堡", "薯條", "可樂"]
+        for x in order:
+            if x not in items:
+                items.append(x)
+
+        for x in items:
+            print(f"{x}:{order.count(x)}")
+
+        break
     else:
-        if num < 1 or num > 3:
-            print("輸入錯誤查無此選項，請重新輸入")
-            continue
-        else:
-            if num == 1:
-                name = input("請輸入餐點名稱: ")
-                menu.append(name)
-                print(menu)
-            elif num == 2:
-                name = input("請輸入要刪除的餐點名稱: ")
-                for i in range(menu.count(name)):
-                    menu.remove(name)
-                print(menu)
-            elif num == 3:
-                for i in range(len(menu)):
-                    if menu.count(name) > 1:
-                        for j in range(menu.count(name)):
-                            menu.remove(name)
-                    else:
-                        print(f"{menu[i]}:{menu.count(menu[i])}")
+        print("請輸入錯誤，請重新輸入")
--- class12/tempCodeRunnerFile.py
+++ class12/tempCodeRunnerFile.py
@@ -1,25 +1,32 @@
 order = []
+
+
 while True:
-    print(order)
-    print("1.新增餐點")
+    print(f"目前order={order}")
+    print("1. 新增餐點")
     print("2. 刪除餐點")
     print("3. 提交菜單")
-    i = int(input("請選擇功能:"))
-    if i == 1:
-        print("請輸入餐點名稱:")
-        name = input()
-        print("請輸入餐點數量:")
-        num = int(input())
-        order.append([name, num])
-    elif i == 2:
-        print("請輸入要刪除的餐點名稱:")
-        name = input()
-        for i in range(len(order)):
-            if order[i][0] == name:
-                order.pop(i)
-                break
-    elif i == 3:
-        print("漢堡:", order[0][1])
-        print("薯條:", order[1][1])
-        print("可樂:", order[2][1])
+
+    choice = input("請選擇功能(1-3): ")
+    if choice == "1":
+        item = input("請輸入餐點名稱: ")
+        order.append(item)
+    elif choice == "2":
+        item = input("請輸入餐點名稱: ")
+        if item in order:
+            for x in range(order.count(item)):
+                order.remove(item)
+    elif choice == "3":
+        items = (
+            []
+        )  # order=["漢堡", "薯條", "可樂", "可樂", "可樂"] -> items=["漢堡", "薯條", "可樂"]
+        for x in order:
+            if x not in items:
+                items.append(x)
+
+        for x in items:
+            print(f"{x}:{order.count(x)}")
+
         break
+    else:
+        print("請輸入錯誤，請重新輸入")
